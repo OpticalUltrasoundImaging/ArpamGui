9f0059687bd83200a4bab1da75b84e758ccb64b1
tnie <tnie@wustl.edu>
2024-04-15 20:55:28 -0500

    fixed cuda hilbert stream sync

--- libuspam/src/cudaSignal.cu
+++ libuspam/src/cudaSignal.cu
@@ -70,7 +70,7 @@ void hilbert2(const double *x, double *out, const int rows, const int cols) {
   CUDA_RT_CALL(cudaMalloc(&d_imag, rows * cols * sizeof(double)));
 
   CUDA_RT_CALL(cudaMemcpyAsync(d_real, x, rows * cols * sizeof(double),
-                               cudaMemcpyHostToDevice));
+                               cudaMemcpyHostToDevice, stream));
 
   // Forward fft
   CUFFT_CALL(cufftExecD2Z(planr2c, d_real, d_cx));
@@ -79,23 +79,20 @@ void hilbert2(const double *x, double *out, const int rows, const int cols) {
   dim3 threadsPerBlock(16, 16);
   dim3 numBlocks((rows + threadsPerBlock.x - 1) / threadsPerBlock.x,
                  (cols + threadsPerBlock.y - 1) / threadsPerBlock.y);
-  kernelHilbert_r2c_freq_switch<<<numBlocks, threadsPerBlock>>>(d_cx, rows,
-                                                                cols);
+  kernelHilbert_r2c_freq_switch<<<numBlocks, threadsPerBlock, 0, stream>>>(
+      d_cx, rows, cols);
 
   // Backward fft
   CUFFT_CALL(cufftExecZ2D(planc2r, d_cx, d_imag));
 
-  kernelHilbert_r2c_scale_and_abs<<<numBlocks, threadsPerBlock>>>(
+  kernelHilbert_r2c_scale_and_abs<<<numBlocks, threadsPerBlock, 0, stream>>>(
       d_real, d_imag, rows, cols);
 
   // Copy to output
   CUDA_RT_CALL(cudaMemcpyAsync(out, d_real, rows * cols * sizeof(double),
-                               cudaMemcpyDeviceToHost));
+                               cudaMemcpyDeviceToHost, stream));
 
-  // CUDA_RT_CALL(cudaMemcpy(out, d_imag, rows * cols * sizeof(double),
-  //                         cudaMemcpyDeviceToHost));
-
-  cudaDeviceSynchronize();
+  CUDA_RT_CALL(cudaStreamSynchronize(stream));
 
   // Cleanup
   CUDA_RT_CALL(cudaFree(d_cx));
@@ -106,72 +103,5 @@ void hilbert2(const double *x, double *out, const int rows, const int cols) {
   CUDA_RT_CALL(cufftDestroy(planc2r));
 
   CUDA_RT_CALL(cudaStreamDestroy(stream));
-
 }
 
-// void hilbert2_v2(const double *x, double *out, const int rows,
-//                        const int cols) {
-//
-//   // Create cuFFT plans
-//   cufftHandle plan_f, plan_b;
-//   cudaStream_t stream = nullptr;
-//
-//   CUFFT_CALL(cufftCreate(&plan_b));
-//   CUFFT_CALL(cufftCreate(&plan_f));
-//
-//   std::array<int, 1> fft_size{1};
-//   const int dist = rows;
-//   const int stride = 1;
-//
-//   // clang-format off
-//   CUFFT_CALL(cufftPlanMany(&plan_f, fft_size.size(), fft_size.data(),
-//                            nullptr, stride, dist,
-//                            nullptr, stride, dist,
-//                            CUFFT_D2Z, cols));
-//
-//   CUFFT_CALL(cufftPlanMany(&plan_b, fft_size.size(), fft_size.data(),
-//                            nullptr, stride, dist,
-//                            nullptr, stride, dist,
-//                            CUFFT_Z2D, cols));
-//   // clang-format on
-//
-//   CUFFT_CALL(cufftSetStream(plan_f, stream));
-//   CUFFT_CALL(cufftSetStream(plan_b, stream));
-//
-//   // Create device arrays
-//   // (inplace transform)
-//   using Real = double;
-//   using Complex = std::complex<Real>;
-//   cufftDoubleComplex *d_cx;
-//
-//   CUDA_RT_CALL(cudaMalloc(&d_cx, rows * cols * sizeof(cufftDoubleComplex)));
-//   CUDA_RT_CALL(cudaMemcpyAsync(d_real, x, rows * cols * sizeof(double),
-//                                cudaMemcpyHostToDevice));
-//
-//   // Forward fft
-//   CUFFT_CALL(cufftExecD2Z(plan_f, d_real, d_cx));
-//
-//   // Manipulate spectrum for Hilbert transform
-//   dim3 threadsPerBlock(16, 16);
-//   dim3 numBlocks((rows + threadsPerBlock.x - 1) / threadsPerBlock.x,
-//                  (cols + threadsPerBlock.y - 1) / threadsPerBlock.y);
-//   kernelHilbert2<<<numBlocks, threadsPerBlock>>>(d_cx, rows, cols);
-//
-//   // Backward fft
-//   CUFFT_ERROR(cufftExecZ2D(plan_b, d_cx, d_real));
-//
-//   const double fct = 1. / rows;
-//   kernelAbs2<<<numBlocks, threadsPerBlock>>>(d_cx, d_real, rows, cols);
-//
-//   // Copy to output
-//   CUDA_RT_CALL(cudaMemcpy(out, d_real, rows * cols * sizeof(double),
-//                           cudaMemcpyDeviceToHost));
-//
-//   cudaDeviceSynchronize();
-//
-//   // Cleanup
-//   CUDA_RT_CALL(cudaFree(d_cx));
-//   CUDA_RT_CALL(cudaFree(d_real));
-//   CUFFT_CALL(cufftDestroy(plan_f));
-//   CUFFT_CALL(cufftDestroy(plan_b));
-// }
